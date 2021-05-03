b95d8a1c4e2a3d717b2a125928c3055d2e7cebb4
Helmi Jauhar Irsyad <helmijauhar8@gmail.com>
2021-05-03 13:13:28 +0700

    first commit

---
+++ Hello.java
@@ -0,0 +1,9 @@
+public class Hello {
+    public void Hai(){
+        System.out.println("Hello World");
+    }
+    public static void main(String[] args) {
+        Hello h = new Hello();
+        h.Hai();
+    }
+}
