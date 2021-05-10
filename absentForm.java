a43111e4ffe723bad1e36c00fc5a2dae9931167e
Helmi Jauhar Irsyad <helmijauhar8@gmail.com>
2021-05-10 08:27:47 +0700

    Update Form Judul + Button List Absent

--- absentForm.java
+++ absentForm.java
@@ -2,37 +2,46 @@ import java.awt.event.*;
 import javax.swing.*;
 
 public class absentForm implements ActionListener {
+    private JLabel judul;
     private JLabel userName;
     private JLabel passWord;
     private JTextField hasilUser;
     private JTextField hasilPass;
     private JButton btnSubmit;
+    private JButton btnList;
 
     public absentForm(){
         JFrame frame = new JFrame();
-        frame.setBounds(200, 200, 600, 350);
-        frame.setTitle("Sistem Absensi Mahhasiswa");
+        frame.setBounds(200, 200, 800, 250);
+        frame.setTitle("Sistem Absensi Mahasiswa");
+
+        judul = new JLabel("Absensi Mahasiswa");
+        judul.setBounds(25, 15, 150, 25);
+        frame.add(judul);
 
         userName = new JLabel("Inputkan Username");
-        userName.setBounds(25, 25, 150, 25);
+        userName.setBounds(25, 45, 150, 25);
         frame.add(userName);
         
-
         passWord = new JLabel("Inputkan Password");
-        passWord.setBounds(25, 50, 150, 25);
+        passWord.setBounds(25, 70, 150, 25);
         frame.add(passWord);
 
         hasilUser = new JTextField();
-        hasilUser.setBounds(150, 25, 150, 25);
+        hasilUser.setBounds(150, 45, 150, 25);
         frame.add(hasilUser);
 
         hasilPass = new JTextField();
-        hasilPass.setBounds(150, 50, 150, 25);
+        hasilPass.setBounds(150, 70, 150, 25);
         frame.add(hasilPass);
 
         btnSubmit = new JButton("Submit");
-        btnSubmit.setBounds(25, 95, 150, 25);
+        btnSubmit.setBounds(25, 115, 100, 25);
         frame.add(btnSubmit);
+
+        btnList = new JButton("List Absent");
+        btnList.setBounds(200, 115, 100, 25);
+        frame.add(btnList);
 
         frame.setLayout(null);
         frame.setVisible(true);
